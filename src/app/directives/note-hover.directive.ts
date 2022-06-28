fb433cfb264eee880b2212a6f1af55e41a4b620c
Taylor Jay Cuevas <taylorjay47@live.com>
2022-06-27 22:51:54 -0600

    fix sharp notes

--- src/app/directives/note-hover.directive.ts
+++ src/app/directives/note-hover.directive.ts
@@ -18,15 +18,27 @@ export class NoteDirective {
   }
 
   @HostListener('click') onClick() {
-    if ($('.' + this.note).css('opacity') === '0.15'){
-      $('.' + this.note).css('opacity', '');
+    if (this.note.length === 2) {
+      if ($('.' + this.note.charAt(0) + '\\#').css('opacity') === '0.15'){
+        console.log('Enabling all ' + this.note + ' notes.')
+        $('.' + this.note.charAt(0) + '\\#').css('opacity', '');
+      } else {
+        console.log('Disabling all ' + this.note + ' notes.')
+        $('.' + this.note.charAt(0) + '\\#').css('opacity', '15%');
+      }
     } else {
-      $('.' + this.note).css('opacity', '15%');
+      if ($('.' + this.note).css('opacity') === '0.15'){
+        console.log('Enabling all ' + this.note + ' notes.')
+        $('.' + this.note).css('opacity', '');
+      } else {
+        console.log('Disabling all ' + this.note + ' notes.')
+        $('.' + this.note).css('opacity', '15%');
+      }
     }
   }
 
   private highlight(color: string) {
     this.el.nativeElement.style.backgroundColor = color;
-    $('.' + this.note).css('background-color',color);
+    this.note.length === 2? $('.' + this.note.charAt(0) + '\\#').css('background-color',color) : $('.' + this.note).css('background-color',color);
   }
 }
