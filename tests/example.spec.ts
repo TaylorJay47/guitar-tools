cb187d5fd642fa88eeba906d15c1f556aeab01e2
Taylor Jay Cuevas <taylorjay47@live.com>
2024-09-17 13:55:34 -0600

    Configure playwright and add github actions

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:4200/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/JayTools/);
 });
 
 test('get started link', async ({ page }) => {
